b0c602a651ded1d1b299e00559add7a8239fe333
Mags <4146037+MagsMagnoli@users.noreply.github.com>
2024-10-29 23:36:34 -0400

    add react query

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -1,15 +1,20 @@
 import { AppSidebar } from '@/components/Sidebar'
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode } from 'react'
+
+const queryClient = new QueryClient()
 
 export const AppShell = ({ children }: { children: ReactNode }) => {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
-    </SidebarProvider>
+    <QueryClientProvider client={queryClient}>
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+          {children}
+        </main>
+      </SidebarProvider>
+    </QueryClientProvider>
   )
 }
