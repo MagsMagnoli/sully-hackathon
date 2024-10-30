cf25ab4e969d32e77260d17f39b4d5b8c89fc0e5
Mags <4146037+MagsMagnoli@users.noreply.github.com>
2024-10-30 00:41:39 -0400

    add conv failed alert

--- components/ConversationList.tsx
+++ components/ConversationList.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import {
   SidebarMenu,
   SidebarMenuButton,
@@ -8,23 +9,33 @@ import {
 import { Skeleton } from '@/components/ui/skeleton'
 import { Conversation } from '@/lib/models'
 import { useQuery } from '@tanstack/react-query'
+import { AlertCircle } from 'lucide-react'
 import Link from 'next/link'
+
+const fetchConversations = async (): Promise<Conversation[]> => {
+  const response = await fetch('/api/conversations')
+  if (response.ok) {
+    throw new Error('Failed to fetch conversations')
+  }
+  return response.json()
+}
 
 export const ConversationList = () => {
   const query = useQuery({
     queryKey: ['conversations'],
-    queryFn: async () => {
-      const response = await fetch('/api/conversations')
-      return response.json()
-    },
+    queryFn: fetchConversations,
   })
 
   if (query.isLoading) {
     return <ConversationsLoadingSkeleton />
   }
 
+  if (query.isError) {
+    return <ConversationsLoadingFailedAlert />
+  }
+
   return (
-    <SidebarMenu>
+    <SidebarMenu data-testid="conversation-list">
       {query.data?.map((conversation: Conversation) => (
         <SidebarMenuItem key={conversation.id}>
           <SidebarMenuButton asChild>
@@ -46,5 +57,15 @@ const ConversationsLoadingSkeleton = ({ count = 10 }: { count?: number }) => {
         </div>
       ))}
     </div>
+  )
+}
+
+const ConversationsLoadingFailedAlert = () => {
+  return (
+    <Alert variant="destructive">
+      <AlertCircle className="h-4 w-4" />
+      <AlertTitle>Error</AlertTitle>
+      <AlertDescription>Conversations failed to load</AlertDescription>
+    </Alert>
   )
 }
